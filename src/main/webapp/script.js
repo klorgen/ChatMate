db80d8ceb78fa74e312a2f81b822499b41bf0a30
Kay Sindre Lorgen <kaysl@stud.ntnu.no>
2017-09-27 08:43:29 +0200

    index javascriptfile

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,34 +1,50 @@
 class Slideshow {
     constructor() {
-        this.images = document.querySelector("#images");
+        this.rooms = document.querySelector("#chat");
+        this.user = document.querySelector("#user");
         this.load();
+
     }
+
     load() {
-        fetch('api/store/images')
+        fetch('api/messages/chatrooms')
                 .then(response => {
                     if (response.ok) {
                         return response.json();
                     }
-                    throw new Error("Failed to load list of images");
+                    throw new Error("Failed to load list of chatrooms");
                 })
-                .then(json => this.addImages(json))
+                .then(json => this.addChatRooms(json))
                 .catch(e => console.log("Error: " + e.message));
     }
 
-    addImages(json) {
-        this.images.innerHTML = '';
+    addChatRooms(json) {
+        this.rooms.innerHTML = '';
         for (let i = 0; i < json.length; i++) {
-            
-            let li = document.createElement('li');
-            li.src = 'api/store/' + json[i].name + "?width=250";
+
+            let ul = document.createElement('ul');
+
 
             let a = document.createElement('a');
-            a.href = "photo.html?photo=" + json[i].name;
-            a.innerHTML = json[i].name + json[i].da;
-            a.appendChild(li);
+            a.href = "chat.html?room=" + json[i].id + "&user=" + this.user.value;
+            a.innerHTML = "#" + json[i].id + " Last changed: " + json[i].version;
+            a.appendChild(ul);
 
-            this.images.appendChild(a);
+            this.rooms.appendChild(a);
         }
     }
+
 }
 let slideshow = new Slideshow();
+
+function addNewChatroom() {
+    this.rooms = document.querySelector("#chat");
+
+    elementCount = rooms.childElementCount + 1;
+    window.location = "chat.html?room=chatroom" + elementCount;
+}
+
+function updateUser(value) {
+    this.user = value;
+    console.log(this.user);
+}
