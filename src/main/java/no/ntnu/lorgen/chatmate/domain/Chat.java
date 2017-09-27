740e293e24de0dccbd39f97b831f2a5bcc6cd9b7
Kay Sindre Lorgen <kaysl@stud.ntnu.no>
2017-09-27 08:42:23 +0200

    Chat class

--- src/main/java/no/ntnu/lorgen/chatmate/domain/Chat.java
+++ src/main/java/no/ntnu/lorgen/chatmate/domain/Chat.java
@@ -2,11 +2,14 @@ package no.ntnu.lorgen.chatmate.domain;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Version;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -25,18 +28,32 @@ import lombok.NoArgsConstructor;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
+@NamedQueries({
+    @NamedQuery(name = Chat.QUERY_FINDALL, query = "SELECT c FROM Chat c")
+})
 public class Chat implements Serializable {
-    
+
+    public static final String QUERY_FINDALL = "findAll";
+
     @Id
     String id;
-    
+
     @XmlTransient
     @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     List<Message> messages;
-    
+
     @Version
     Timestamp version;
 
-public Chat(String id){
-    this.id = id;
-}}
+    public Chat(String id) {
+        this.id = id;
+    }
+
+    public List<Message> getMessages() {
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+        return messages;
+    }
+
+}
